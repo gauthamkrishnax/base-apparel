fec28a172e08e2e2c5ea3caaefa8c8f026e2bcc9
Gautham Krishna <gauthamkrishnax@gmail.com>
2021-06-29 17:03:12 +0530

    basic styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/logo.svg";
+import heroDesk from "./assets/hero-desktop.jpg";
+import heroMobile from "./assets/hero-mobile.jpg";
+import "./styles/styles.scss";
+import Email from "./Email";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <img src={logo} alt="Base Apparel Logo" />
+        <img src={heroMobile} alt="female Model" className="hero mobile" />
+        <div className="container">
+          <h1 className="heading">
+            We're <strong>Coming Soon</strong>
+          </h1>
+          <p className="paragraph">
+            Hello fellow shoppers! we're currently building our new fashion
+            store. Add your email below to stay up-to-date with announcements
+            and our launch deals.
+          </p>
+          <Email />
+        </div>
+      </div>
+      <div className="hero-container">
+        <img src={heroDesk} alt="female Model" className="hero desktop" />
+      </div>
     </div>
   );
 }
---
+++ src/Email.js
@@ -0,0 +1,9 @@
+const Email = () => {
+  return (
+    <div>
+      <p>Email</p>
+    </div>
+  );
+};
+
+export default Email;
